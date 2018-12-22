643c3d1f94f277d2dcba277e3d246952cf16048c
Marcv0dka <46052344+Marcv0dka@users.noreply.github.com>
2018-12-22 01:23:15 +0100

    Add files via upload

---
+++ NewHeists/ArmoredTruck.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using GTA;
+using GTA.Math;
+using GTA.Native;
+
+namespace NewHeists
+{
+    public class ArmoredTruck : Script
+    {
+        private List<Vehicle> listVehicle = new List<Vehicle>();
+        private List<Blip> listBlip = new List<Blip>();
+        private bool enabled = true;
+
+        public ArmoredTruck()
+        {
+            this.KeyDown += onKeyDown;
+        }
+
+        private void onKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.NumPad1)
+            {
+                Vector3 safePos = World.GetNextPositionOnStreet(Game.Player.Character.GetOffsetInWorldCoords(new Vector3(80, 80, 50)), true);
+
+                Vehicle car = World.CreateVehicle(VehicleHash.Stockade, safePos, getRoadHeading(safePos));
+                Blip carBlip = car.AddBlip();
+                carBlip.Scale = 0.5f;
+                listVehicle.Add(car);
+                listBlip.Add(carBlip);
+                UI.Notify(listVehicle.ToString());
+            }
+            if (e.KeyCode == Keys.NumPad3)
+            {
+                UI.Notify("" + enabled.ToString());
+            }
+        }
+
+        private float getRoadHeading(Vector3 coords)
+        {
+            //Vector3 coords = Game.Player.Character.Position;
+            Vector3 closestVehicleNodeCoords;
+            float roadheading;
+
+            OutputArgument tempcoords = new OutputArgument();
+            OutputArgument temproadheading = new OutputArgument();
+
+            Function.Call<Vector3>(Hash.GET_CLOSEST_VEHICLE_NODE_WITH_HEADING, coords.X, coords.Y, coords.Z, tempcoords, temproadheading, 1, 3, 0);
+            closestVehicleNodeCoords = tempcoords.GetResult<Vector3>();
+            roadheading = temproadheading.GetResult<float>();
+            return roadheading;
+        }
+
+        private void removeVehicles()
+        {
+            for (int i = 0; i < listVehicle.Count(); i++)
+            {
+                listVehicle.ElementAt(i).Delete();
+            }
+            listVehicle.Clear();
+        }
+
+        private void removeBlips()
+        {
+            for (int i = 0; i < listBlip.Count(); i++)
+            {
+                listBlip.ElementAt(i).Remove();
+            }
+            listBlip.Clear();
+        }
+
+        public void disableArmoredTrucks()
+        {
+            this.removeBlips();
+            this.removeVehicles();
+            UI.Notify("removed");
+        }
+
+        public void setEnabled(bool enabled)
+        {
+            this.enabled = enabled;
+        }
+
+    }
+}
---
+++ NewHeists/Main.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GTA;
+using GTA.Math;
+using System.Windows.Forms;
+using System.Drawing;
+using GTA.Native;
+
+namespace NewHeists
+{
+    public class Main : Script
+    {
+        private ArmoredTruck at = new ArmoredTruck();
+        private ScriptSettings config;
+        private Keys enableKey;
+        private bool enabled = true;
+
+        public Main()
+        {
+            this.Tick += onTick;
+            this.KeyDown += onKeyDown;
+            this.Aborted += onAborted;
+            enableMod();
+        }
+
+        private void onAborted(object sender, EventArgs e)
+        {
+            disableMod();
+            UI.Notify("~r~NewHeists ~w~Reloaded");
+        }
+
+        private void onTick(object sender, EventArgs e)
+        {
+            this.Interval = 10;
+        }
+
+        private void onKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == this.enableKey)
+            {
+                if (!enabled)
+                {
+                    at.setEnabled(true);
+                    enableMod();
+                } else
+                {
+                    at.setEnabled(false);
+                    disableMod();
+                }
+            }
+        }
+
+        private void enableMod()
+        {
+            enabled = true;
+            loadSettings();
+            sendMSG("NewHeists loaded", "successfully", "I have some work for you. Come over!", 1, "CHAR_LESTER");
+        }
+
+        private void disableMod()
+        {
+            enabled = false;
+            sendMSG("NewHeists unloaded", "successfully", "WE WERE BUSTED. FUCK OFF!", 1, "CHAR_LESTER");
+        }
+
+        private void loadSettings()
+        {
+            this.config = ScriptSettings.Load("scripts\\NewHeists.ini");
+            this.enableKey = (Keys)this.config.GetValue<Keys>("Options", "EnableModKey", Keys.PageDown);
+        }
+
+        public void sendMSG(string sender, string subject, string text, int icon, string texture = "CHAR_DEFAULT")
+        {
+
+            GTA.Native.Function.Call(Hash._SET_NOTIFICATION_TEXT_ENTRY, new InputArgument[] { "STRING" });
+            GTA.Native.Function.Call(Hash._ADD_TEXT_COMPONENT_STRING, new InputArgument[] { text });
+            GTA.Native.Function.Call(Hash._SET_NOTIFICATION_MESSAGE, new InputArgument[] { texture, texture, 1, icon, sender, subject });
+        }
+    }
+}
